8b426a18c8f9cf1b1fa54588dcf5548fe679f4fd
Marcos Godoy <mjgb_4everalone@hotmail.com>
2024-05-17 09:02:07 -0600

    images optimization

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -13,67 +13,67 @@ import "yet-another-react-lightbox/plugins/thumbnails.css"
 
 const images = [
   {
-     src: "/gallery/image (1).jpg",
+     src: "/gallery/image (1).webp",
      width: 532,
      height: 355,
   },
   {
-    src: "/gallery/image (2).jpg",
+    src: "/gallery/image (2).webp",
     width: 590,
     height: 410,
   },
   {
-    src: "/gallery/image (3).jpg",
+    src: "/gallery/image (3).webp",
     width: 576,
     height: 383,
   },
   {
-    src: "/gallery/image (4).jpg",
+    src: "/gallery/image (4).webp",
     width: 450,
     height: 300,
   },
   {
-    src: "/gallery/image (5).jpg",
+    src: "/gallery/image (5).webp",
     width: 471,
     height: 706,
   },
   {
-    src: "/gallery/image (6).jpg",
+    src: "/gallery/image (6).webp",
     width: 216,
     height: 384,
   },
   {
-    src: "/gallery/image (7).jpg",
+    src: "/gallery/image (7).webp",
     width: 624,
     height: 416,
   },
   {
-    src: "/gallery/image (8).jpg",
+    src: "/gallery/image (8).webp",
     width: 604,
     height: 402,
   },
   {
-    src: "/gallery/image (9).jpg",
+    src: "/gallery/image (9).webp",
     width: 472,
     height: 708,
   },
   {
-    src: "/gallery/image (10).jpg",
+    src: "/gallery/image (10).webp",
     width: 883,
     height: 489,
   },
   {
-    src: "/gallery/image (11).jpg",
+    src: "/gallery/image (11).webp",
     width: 1000,
     height: 695,
   },
   {
-    src: "/gallery/image (12).jpg",
+    src: "/gallery/image (12).webp",
     width: 740,
     height: 493,
   },
   {
-    src: "/gallery/image (13).jpg",
+    src: "/gallery/image (13).webp",
     width: 552,
     height: 367,
   },
