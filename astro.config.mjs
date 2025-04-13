5217cc582e20a8eb5f1d920029544c86a4b5533d
Marcos Jonatan Godoy Blanco <marcosjgodoyb@gmail.com>
2025-04-12 21:01:57 -0600

    google ads tracking added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -7,5 +7,14 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown()]
+  site: "https://wwww.geriatriccaresv.com",
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
